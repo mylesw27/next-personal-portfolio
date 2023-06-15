c640c489823177740f4e38eb6e73a2a5f8fc4b44
mylesw27 <myleswiegel27@gmail.com>
2023-06-15 16:03:01 -0700

    init commit

--- app/page.js
+++ app/page.js
@@ -4,40 +4,22 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <div class={styles.center}>
+        <div className={styles.description}>
+          <h1>Myles <div></div> Wiegel</h1>
         </div>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className={styles.about}>
+        <div className={styles.aboutdescription}>
+          <h2>About Me</h2>
+          <p>I am an accountant turned software engineer.</p>
+          <p>I spent the last six years in accounting but decided it was time to change my career trajectory.</p>
+          <p>With a passion for front end design, I am ready to bring</p>
+        </div>
+        <img src='headshot.png' className={styles.headshot}/>
       </div>
+
 
       <div className={styles.grid}>
         <a
